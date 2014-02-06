efecfcfcc3710ec16d221c4ddd9a41b17b2a8c94
Andrew Shi <ashilol95@gmail.com>
2014-02-05 18:00:33 -0800

    Commit

--- RopesPlannerApp/src/planner/application/MainActivity.java
+++ RopesPlannerApp/src/planner/application/MainActivity.java
@@ -1,8 +1,11 @@
 package planner.application;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        registerForContextMenu(button);
 	}
 
 	@Override
@@ -18,5 +23,11 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, view, menuInfo);
+        menu.setHeaderTitle("Android Context Menu");
+        menu.add(0, view.getId(), 0, "Invoke Context Function 1");
+        menu.add(0, view.getId(), 0, "Invoke Context Function 2");
 
 }
