1a9c3309b7769049b92e99c1a5a872b22ac9db96
Andrew Shi <ashilol95@gmail.com>
2014-02-13 20:24:20 -0800

    Commit

--- RopesPlannerApp/src/planner/application/MainActivity.java
+++ RopesPlannerApp/src/planner/application/MainActivity.java
@@ -1,16 +1,19 @@
 package planner.application;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends Activity {
-
+    public final static String EXTRA_MESSAGE = "planner.application.MESSAGE";
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button);
@@ -24,6 +27,14 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+}
+    public void sendMessage(View view) {
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
         super.onCreateContextMenu(menu, view, menuInfo);
         menu.setHeaderTitle("Android Context Menu");
